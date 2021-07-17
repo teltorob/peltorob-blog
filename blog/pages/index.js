8c32350f195cdc15787aa5d600992055093fac9a
teltorob <teltorob@gmail.com>
2021-07-18 03:21:56 +0530

    Taiwind setup, ssr working

--- blog/pages/index.js
+++ blog/pages/index.js
@@ -2,5 +2,5 @@ import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Ayush lord</h1>;
+  return <h1 class="text-4xl text-center font-bold">Lord Ayush</h1>;
 }
