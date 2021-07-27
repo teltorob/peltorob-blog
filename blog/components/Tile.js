9c72cbdcc2b417683610ba077b9c05eb0ce162fa
teltorob <teltorob@gmail.com>
2021-07-27 19:51:58 +0530

    responsive tile created

---
+++ blog/components/Tile.js
@@ -0,0 +1,15 @@
+export default function Tile() {
+  return (
+    <div className=" mb-10 p-10 w-2/5 min-h-72 bg-gray-100 rounded-2xl">
+      <h2 className=" text-xl font-semibold text-gray-800 pb-8">
+        The unfair advantage of overconfidence
+      </h2>
+      <p className=" text-base text-gray-700 font-regular pb-8">
+        When it comes to showcasing your work, there are various hurdles one
+        needs to cross. This blogpost goes through my story of exploring the
+        possibilities that showing off work generates.
+      </p>
+      <h3 className=" text-gray-800 font-medium">Read More</h3>
+    </div>
+  );
+}
---
+++ blog/pages/test.js
@@ -0,0 +1,12 @@
+import Tile from "../components/Tile";
+
+export default function Test() {
+  return (
+    <>
+      <Tile />
+      <Tile />
+      <Tile />
+      <Tile />
+    </>
+  );
+}
