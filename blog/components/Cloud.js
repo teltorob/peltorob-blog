6b46a90ebdfa67af5796f0708e160015dccfb14b
teltorob <teltorob@gmail.com>
2021-08-02 22:20:16 +0530

    random cloud animation added

--- blog/components/Cloud.js
+++ blog/components/Cloud.js
@@ -1,11 +1,58 @@
-import ExCloud from "../clouds/cloud2.svg";
+import Cloud1 from "../clouds/cloud1.svg";
+import Cloud2 from "../clouds/cloud2.svg";
+import Cloud3 from "../clouds/cloud3.svg";
+import Cloud4 from "../clouds/cloud4.svg";
+import { motion } from "framer-motion";
 
 const heights = ["2", "6", "12", "28", "36", "40", "64", "72"];
 const Cloud = () => {
   return (
-    <div className={`absolute z-20 ${RandomPos()}`}>
-      <ExCloud />
-    </div>
+    <>
+      <motion.div
+        initial={{ x: "-50rem" }}
+        animate={{ x: 1800 }}
+        transition={{
+          repeat: Infinity,
+          duration: 30,
+        }}
+        className={` absolute z-20  ${RandomPos()} `}
+      >
+        <Cloud1 />
+      </motion.div>
+      <motion.div
+        initial={{ x: "-20rem" }}
+        animate={{ x: 1800 }}
+        transition={{
+          repeat: Infinity,
+          duration: 30,
+        }}
+        className={` absolute z-20  ${RandomPos()} `}
+      >
+        <Cloud2 />
+      </motion.div>
+      <motion.div
+        initial={{ x: "-50rem" }}
+        animate={{ x: 1800 }}
+        transition={{
+          repeat: Infinity,
+          duration: 30,
+        }}
+        className={` absolute z-20  ${RandomPos()} `}
+      >
+        <Cloud3 />
+      </motion.div>
+      <motion.div
+        initial={{ x: "-60rem" }}
+        animate={{ x: 1800 }}
+        transition={{
+          repeat: Infinity,
+          duration: 30,
+        }}
+        className={` absolute z-20  ${RandomPos()} `}
+      >
+        <Cloud4 />
+      </motion.div>
+    </>
   );
 };
 
